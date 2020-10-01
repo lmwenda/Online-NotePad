0eb11123da9fb438d069effa0d62c3a65aaf64f0
spartenwolffe <67679722+spartenwolffe@users.noreply.github.com>
2020-10-01 19:28:01 +0100

    Added a few more Interactive Options
    You can now use the About us Option and the Open File Option

--- src/com/company/app.java
+++ src/com/company/app.java
@@ -12,6 +12,7 @@ public class app {
     private static JMenu menu;
     private static JMenu submenu;
     private static JMenu about;
+    private static JFileChooser fileChooser;
     private static JMenuItem i1, i2, i3, i4, i5, i6;
 
     public static void main(String[] args) {
@@ -45,21 +46,36 @@ public class app {
         // SubMenu's
         i1=new JMenuItem("Save");
         i2=new JMenuItem("Save as");
-        i3=new JMenuItem("Save all");
+        i3=new JMenuItem("Open");
         i4=new JMenuItem("New File");
-        i5=new JMenuItem("Find Help");
+        i5=new JMenuItem("About us");
         i6=new JMenuItem("Settings");
 
         // Action Liseners
         i2.addActionListener(action ->{
-            JFileChooser fileChooser = new JFileChooser();
+            fileChooser = new JFileChooser();
             fileChooser.setDialogTitle("Specify a text file to save.");
 
             int userSelection = fileChooser.showSaveDialog(frame);
             if (userSelection == JFileChooser.APPROVE_OPTION) {
                 File fileToSave = fileChooser.getSelectedFile();
+                JOptionPane.showConfirmDialog(null, fileToSave.getAbsoluteFile());
                 System.out.println("Save as file: " + fileToSave.getAbsolutePath());
             }
+        });
+        
+        i3.addActionListener(action -> {
+        	JFileChooser fileChooser = new JFileChooser();
+        	fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        	int result = fileChooser.showOpenDialog(frame);
+        	if (result == JFileChooser.APPROVE_OPTION) {
+        	    File selectedFile = fileChooser.getSelectedFile();
+        	    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+        	}
+        });
+        
+        i5.addActionListener(action -> {
+        	JOptionPane.showMessageDialog(frame.getComponent(0), "I am a new Java Developer who's creating many Projects just like this if you like this and want it to be continued even More. Consider Contributing to our Development Team. Thanks :) and bye.\"", "About us", JOptionPane.INFORMATION_MESSAGE);
         });
 
         // Adding the Menu's together
